85b5e096a0e6d0ddff3426b29932a9436f27027e
jbutle55 <justin.butler12@hotmail.ca>
2022-04-14 11:13:22 -0600

    debug print

--- detect.py
+++ detect.py
@@ -121,6 +121,7 @@ def run(
         visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
         save_fms = save_dir / Path(path).stem / 'fms'
         if not os.path.exists(save_fms):
+            print(f'Making fms dir')
             os.mkdir(save_fms)
         print(model)
         pred = model(im, augment=augment, visualize=visualize, save_fms=save_fms)
