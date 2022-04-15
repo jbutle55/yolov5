1c9a0671a581f82f580b0345706bb6f8bb58e06b
jbutle55 <justin.butler12@hotmail.ca>
2022-04-15 09:17:10 -0600

    re-enable printing during fms saving

--- utils/plots.py
+++ utils/plots.py
@@ -162,7 +162,7 @@ def feature_saving(x, module_type, stage, save_dir=Path('runs/detect/exp')):
             for b in range(batch):
                 for i in range(channels):
                     f = save_dir / f"stage{stage}_{module_type.split('.')[-1]}_batch_{b}_feature_{i}.npy"  # filename
-                    # print(f"Saving {f}")
+                    print(f"Saving {f}")
                     np.save(str(f.with_suffix('.npy')), x[b, i].cpu().detach().numpy())  # npy save
     return
 
