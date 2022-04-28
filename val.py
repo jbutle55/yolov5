1b308762c3cd7864bfbe6d8a7a19d67793e0b7cb
jbutle55 <justin.butler12@hotmail.ca>
2022-04-28 13:57:30 -0600

    remove debug printing

--- val.py
+++ val.py
@@ -157,7 +157,7 @@ def run(
     cuda = device.type != 'cpu'
     is_coco = isinstance(data.get('val'), str) and data['val'].endswith('coco/val2017.txt')  # COCO dataset
     print(is_coco)
-    is_coco = False
+    is_coco = True
     nc = 1 if single_cls else int(data['nc'])  # number of classes
     iouv = torch.linspace(0.5, 0.95, 10, device=device)  # iou vector for mAP@0.5:0.95
     niou = iouv.numel()
