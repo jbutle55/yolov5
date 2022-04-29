0792e5f16f9cfb268ef98c60b145d717e4acc51e
jbutle55 <justin.butler12@hotmail.ca>
2022-04-29 13:56:28 -0600

    debugging json saving

--- val.py
+++ val.py
@@ -61,7 +61,7 @@ def save_one_json(predn, jdict, path, class_map):
     image_id = int(path.stem) if path.stem.isnumeric() else path.stem
     box = xyxy2xywh(predn[:, :4])  # xywh
     # box = predn[:, :4] # x1,y1,x2,y2
-    # box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
+    box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
     for p, b in zip(predn.tolist(), box.tolist()):
         jdict.append({
             'image_id': image_id,
