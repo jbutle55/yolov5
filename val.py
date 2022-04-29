ef446b0cca022228ae49be3b85c39b31441d4e63
jbutle55 <justin.butler12@hotmail.ca>
2022-04-29 14:07:35 -0600

    debugging json saving

--- val.py
+++ val.py
@@ -302,7 +302,7 @@ def run(
         w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
         # anno_json = str(Path(data.get('path', '../coco')) / 'annotations/instances_val2017.json')  # annotations json
         # anno_json = '/Users/justinbutler/Desktop/school/Calgary/ML_Work/Datasets/Shapes/Triangles_640_bw_50imgs/shapes.json'
-        anno_json = '/content/gdrive/MyDrive/UAV-Vehicle-Detection-Dataset/cars_only/dataset4/dataset4_fixcars.json'
+        anno_json = '/content/gdrive/MyDrive/UAV-Vehicle-Detection-Dataset/cars_only/dataset4/dataset4_x1y1wh.json'
         # anno_json = f'{dataloader.dataset.path[:-7]}/shapes.json'  # Remove images from pathname
         print(f'Using annotation file: {anno_json}')
 
