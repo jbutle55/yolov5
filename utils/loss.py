df664ffa405cc33497c25a2100e9e4e2cf50bbbc
jbutle55 <justin.butler12@hotmail.ca>
2022-05-11 14:55:44 -0600

    target debugging

--- utils/loss.py
+++ utils/loss.py
@@ -212,14 +212,21 @@ class ComputeLoss:
 
             # Match targets to anchors
             t = targets * gain  # shape(3,n,7)
-            print(f't: {t}')
+            print(f't[0][0]: {t[0][0]}')
             print(f't shape: {t.shape}')
+            print(f't[..., 4:6] = {t[..., 4:6]}')
             if nt:
                 # Matches
                 r = t[..., 4:6] / anchors[:, None]  # wh ratio
-                print(f'r: {r}')
+
+                gt_areas = t[..., 4] * t[..., 5]
+                print(f'gt_areas: {gt_areas}')
+                print(f' gt areas shape: {gt_areas.shape}')
+
+                print(f'r[0]: {r[0]}')
                 print(f'r shape: {r.shape}')
                 j = torch.max(r, 1 / r).max(2)[0] < self.hyp['anchor_t']  # compare
+                print(f'j: {j}')
                 # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
                 t = t[j]  # filter
                 filt_targets = targets[j]
