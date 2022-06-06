5a23701144b60eafc4644dd42f02f04f4f4ba672
jbutle55 <justin.butler12@hotmail.ca>
2022-06-06 08:20:02 -0600

    use shapes json val file

--- val.py
+++ val.py
@@ -303,7 +303,7 @@ def run(
     if save_json and len(jdict):
         w = Path(weights[0] if isinstance(weights, list) else weights).stem if weights is not None else ''  # weights
         # anno_json = str(Path(data.get('path', '../coco')) / 'annotations/instances_val2017.json')  # annotations json
-        anno_json = '/home/justin.butler1//Data/shapes/Shapes_1500imgs/no_gauss/shapes.json'
+        anno_json = '/home/justin.butler1//Data/shapes/Shapes_1500imgs_small/no_gauss/shapes.json'
         # anno_json = '/home/justin.butler1/Data/uav-detect/cars-only/dataset4/dataset4_x1y1wh.json'
         # anno_json = '/home/justin.butler1/Data/aerial-cars/cars-only/labels/valid/cars_only_boxmode_wkeys.json'  # Remove images from pathname
         print(f'Using annotation file: {anno_json}')
